a78d92b1fa29ebc3a5095364a463fb9e0fe005cb
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-20 02:29:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,2 @@
-# Write your code here.
-var katzdeli = [];
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return "Welcome" + name + "You are number" + katzDeliLine.length + "in line."
-end
-}
-
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length > 0);
-    return "currently serving" + name + ".";
-  if (katzDeliLine.length === 0)
-    return "There is nobody waiting to be served!";
-end
-  
-  }
-  function currentLine(katzDeliLine) {
-    if (katzDeliLine.length > 0) {
-      for(var i = 0; i < katzDeliLine.length; i++) {
-        katzDeliLine = katzDeliLine.splice(0, -2)
-        return "The line is currently:" + katzDeliLine
-      if (katzDeliLine.length === 0)
-        return "The line is currently empty"
-    end
-  end
   
